bbbb885c3dbeea39b13e1d601c161fe53d8c51ec
amaanabbasi <amaanabbasi99@gmail.com>
2021-10-22 17:08:30 +0530

    Create django-database table definition which will allow you to store student details.

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):  
+    name = models.CharField(max_length=100, )    
+    date_of_birth = models.DateField()  
+    address = models.TextField()     
+    student_phone_number = models.CharField(max_length=10, )
+    student_email = models.EmailField()
+    guardian_phone_number = models.CharField(max_length=10, )
+    guardian_email = models.EmailField()
+    uid = models.CharField(max_length=64, unique=True)
+  
+
+    def __str__(self):
+        return self.name
